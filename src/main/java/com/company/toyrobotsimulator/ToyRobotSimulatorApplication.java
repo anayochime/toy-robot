c204622ec8fef189890d8356113254522f065e2a
anayo chime <anayochime@gmail.com>
2019-07-14 19:00:14 +0100

    Initial app commit

---
+++ src/main/java/com/company/toyrobotsimulator/ToyRobotSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.company.toyrobotsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRobotSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyRobotSimulatorApplication.class, args);
+	}
+
+}
